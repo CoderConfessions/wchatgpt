e40c0f18c8f4902b289ddb7c1dd2945d52665421
xilan <lan.xi.scut@outlook.com>
2023-04-01 18:30:32 +0800

    --log support create_chat_id

--- svr/go.mod
+++ svr/go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.7.0
+	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
 	github.com/sashabaranov/go-openai v1.5.7
 )
---
+++ svr/handler/create_chat_id.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type createChatIdReq struct {
+	UserUID string `json:"usrd_uid"`
+}
+
+type createChatIdData struct {
+	ChatID string `json:"chat_id"`
+}
+
+func createChatId(http_resp http.ResponseWriter, http_req *http.Request) {
+	resp := UniversalResp{}
+	defer response(&resp, http_resp)
+
+	buf, err := ioutil.ReadAll(http_req.Body)
+	if err != nil {
+		http_resp.WriteHeader(http.StatusBadGateway)
+		resp.QuickSetup(NetworkError, fmt.Sprintf("ReadAll error: %s", err.Error()))
+		return
+	}
+
+	internal_req := createChatIdReq{}
+	err = json.Unmarshal(buf, &internal_req)
+	if err != nil {
+		http_resp.WriteHeader(http.StatusBadRequest)
+		resp.QuickSetup(UnmarshalJsonError, fmt.Sprintf("Unmarshal error: %s", err.Error()))
+		return
+	}
+
+	if internal_req.UserUID == "" {
+		http_resp.WriteHeader(http.StatusBadRequest)
+		resp.QuickSetup(ParamaterError, fmt.Sprintf("Parameter error: %s", "some field miss"))
+		return
+	}
+	chatID := uuid.New().String()
+	resp.Data = createChatIdData{ChatID: chatID}
+
+	// TODO: write db record user uid in chat id
+
+	resp.QuickSetup(Ok, "ok")
+	return
+}
